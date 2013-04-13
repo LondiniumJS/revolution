8d399f408833e8779f3119622dc3031f3b6e2e3d
Nicola Greco <notsecurity@gmail.com>
2013-04-13 15:17:52 +0200

    JSON files

---
+++ app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var express = require("express");
+var async = require("async");
+var redis = require("redis");
+var http = require('http');
+var engine = require('ejs-locals');
+
+var data = {
+  extended: require('./json/extended'),
+  graph: require('./json/graph'),
+  topics: require('./json/topics'),
+  twitter_lookup: require('./json/twitter_lookup')
+};
+
+
+var app = module.exports = express();
+
+app.locals({_layoutFile: true});
+
+app.configure(function () {
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "ejs");
+  app.engine('ejs', engine);
+  app.engine("html", engine);
+  app.use(express.bodyParser());
+  app.use(express.cookieParser("alskjald0q9udqokwdmqldiqud0woqijdklq09"));
+  app.use(express.session());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + "/static"));
+  app.use(app.router);
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/api/extended', function(req, res) {
+  res.json(data.extended);
+});
+
+app.get('/api/graph', function(req, res) {
+  res.json(data.graph);
+});
+
+app.get('/api/topics', function(req, res) {
+  res.json(data.topics);
+});
+
+app.get('/api/twitter_lookup', function(req, res) {
+  res.json(data.twitter_lookup);
+});
+
+app.get('*', function(req, res){
+  res.redirect("/");
+});
+
+var port = process.env.PORT || 1200;
+var server = http.createServer(app).listen(port, function () {
+	console.log("Verses server escucha sobre ", server.address().port, app.settings.env);
+});
